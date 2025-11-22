691d160124de9b8652449a6dd279fa4c79d86c97
mzckawa <mzc@cin.ufpe.br>
2025-11-22 15:36:25 -0300

    Arquivo do router

---
+++ app/routers/produto_router.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi import APIRouter
+from typing import List
+from app.schemas.produto_schema import ProdutoRaw, ProdutoClean
+from app.services.produto_service import tratar_produtos
+
+router = APIRouter()
+
+@router.post("/limpar-produtos", response_model=List[ProdutoClean])
+async def tratar_pedidos(dados: List[ProdutoRaw]) -> List[ProdutoClean]:
+    return [tratar_produtos(produto) for produto in dados]
