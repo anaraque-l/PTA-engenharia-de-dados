6dadaef4e3c598ea424ea34f97cfd4b0b1977cf5
Daniel Cavalcanti <danielcmottas@gmail.com>
2025-11-24 12:12:50 -0300

    docs: alteração do prefix e adição de tag na rota dos pedidos

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 app.include_router(vendedor_router, prefix="/vendedores", tags=["Vendedores"])
 app.include_router(itenspedidos_router, prefix="/itens-pedidos", tags=["ItensPedidos"])
 app.include_router(example_router, prefix="/example", tags=["Example"])
-app.include_router(pedidos_router.router, prefix="/api/v1")
+app.include_router(pedidos_router.router, prefix="/pedidos", tags=["Pedidos"])
 # ROTAS FIXAS
 @app.get("/", description="Mensagem de boas-vindas da API.")
 async def read_root():
